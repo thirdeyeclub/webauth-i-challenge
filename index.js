fb65827672f6cb3721b74c376e7e95d72fcf42f3
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-01 17:52:17 -0400

    built file structure

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const server = express();
+const db = require('./database/config00.js');
+//const model = require('./models/model-model.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+//crud and hash
+
+
+/*********************port and env************************* */
+var ns = ['home Indie', 'home Ruby', 'home Chance','Mr. Anderson','home Employee #ER28-0652','friend', 'to the Server','to the happiest server this side of the sea, love','HOST #420'];
+var n = ns[Math.floor(Math.random()*ns.length)];
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () =>
+  console.log(`**Welcome ${n}.**\n** UP and running on http://localhost:${PORT} **\n`)
+);
---
+++ knexfile.js
@@ -0,0 +1,21 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/data.db3',
+    },
+    useNullAsDefault: true, 
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+    // by default SQLite will not enforce foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done); 
+      },
+    },
+  },
+};
